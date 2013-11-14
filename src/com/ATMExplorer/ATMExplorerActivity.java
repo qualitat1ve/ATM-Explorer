8dae871f4b3325875ee7dca49596e3b0936503f6
Maks Kukushkin <maks.kukushkin@gmail.com>
2013-11-14 22:04:27 +0200

    maven support was added

--- src/com/ATMExplorer/ATMExplorerActivity.java
+++ src/com/ATMExplorer/ATMExplorerActivity.java
@@ -11,7 +11,8 @@ import android.widget.ListView;
 public class ATMExplorerActivity extends Activity {
 
     private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    private ListView mDrawerLeftList;
+    private ListView mDrawerRightList;
     private String[] mSampleItems = {"First Item", "Second Item", "Third Item"};
 
     @Override
@@ -20,12 +21,15 @@ public class ATMExplorerActivity extends Activity {
         setContentView(R.layout.top_content_layout);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.top_content_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.list_view_item_layout,mSampleItems));
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mDrawerLeftList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerRightList = (ListView) findViewById(R.id.right_drawer);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_view_item_layout, mSampleItems);
+        mDrawerRightList.setAdapter(adapter);
+        mDrawerLeftList.setAdapter(adapter);
+        mDrawerRightList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                mDrawerLayout.closeDrawer(mDrawerList);
+                mDrawerLayout.closeDrawer(mDrawerRightList);
             }
         });
         getActionBar().setDisplayHomeAsUpEnabled(true);
