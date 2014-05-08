08c65a10552560a7a90694ffc650ea235a8c6bef
Maks Kukushkin <m.kukushkin@samsung.com>
2014-05-08 17:12:57 +0300

    Fixed maps

--- src/com/atmexplorer/ATMExplorerActivity.java
+++ src/com/atmexplorer/ATMExplorerActivity.java
@@ -2,6 +2,7 @@ package com.atmexplorer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.AdapterView;
