866628948ed9ce9109bd67ec868e771b86bff3e5
Maks Kukushkin <m.kukushkin@samsung.com>
2014-05-13 14:01:51 +0300

    removed unused imports

--- src/com/atmexplorer/ATMExplorerActivity.java
+++ src/com/atmexplorer/ATMExplorerActivity.java
@@ -2,12 +2,6 @@ package com.atmexplorer;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.widget.DrawerLayout;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 public class ATMExplorerActivity extends Activity {
 
@@ -15,6 +9,5 @@ public class ATMExplorerActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
     }
 }
