5c91e1841e72fe4dee2494533a12a579a3d9a65d
Maks Kukushkin <m.kukushkin@samsung.com>
2014-07-28 16:25:20 +0300

    [ATM][ActionBar] Remove navigation between list and map fragments from action bar

--- src/main/java/com/atmexplorer/MainActivity.java
+++ src/main/java/com/atmexplorer/MainActivity.java
@@ -17,10 +17,7 @@ import android.view.View;
 import android.view.Gravity;
 import android.widget.Button;
 import android.widget.SearchView;
-import android.widget.Toast;
-import com.atmexplorer.adapter.NavigationAdapter;
 import com.atmexplorer.model.ATMItem;
-import com.atmexplorer.model.SpinnerNavigationItem;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -28,25 +25,18 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author Maks Kukushkin (maks.kukushkin@gmail.com)
  */
 
-public class MainActivity extends Activity implements ActionBar.OnNavigationListener, GoogleMapFragment.OnMapReadyListener {
+public class MainActivity extends Activity implements GoogleMapFragment.OnMapReadyListener {
 
     private ActionBar mActionBar;
-    private List<SpinnerNavigationItem> mNavigationItemList;
-    private NavigationAdapter mNavigationAdapter;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
-    private CharSequence mDrawerTitle;
     private ATMListFragment mATMListFragment;
     private MapFragment mMapFragment;
     private View mDrawerMenu;
-    private int mActionBarIconId;
     private LocationTracker mLocationTracker;
     private boolean isSearchActive;
 
@@ -106,52 +96,27 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
 
     private void setUpDrawer() {
         mDrawerMenu = findViewById(R.id.left_drawer);
-        mDrawerTitle = getResources().getString(R.string.drawer_open_title);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer,
                 R.string.drawer_open_title, R.string.drawer_close_title) {
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
                 mActionBar.setIcon(getResources().getDrawable(R.drawable.ic_logo));
-                mActionBar.setDisplayShowTitleEnabled(false);
-                mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+                mActionBar.setTitle(getString(R.string.drawer_close_title));
                 invalidateOptionsMenu();
             }
 
             public void onDrawerOpened(View view) {
                 super.onDrawerOpened(view);
-                mActionBar.setDisplayShowTitleEnabled(true);
-                if (mActionBarIconId == 0) {
-                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.empty_drawer_message), Toast.LENGTH_SHORT).show();
-                } else {
-                    mActionBar.setIcon(getResources().getDrawable(mActionBarIconId));
-                    mActionBar.setTitle(mDrawerTitle);
-                }
-                mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+                mActionBar.setTitle(getString(R.string.drawer_open_title));
                 invalidateOptionsMenu();
             }
         };
-
         mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     private void setUpActionBar() {
         mActionBar = getActionBar();
-        mActionBar.setDisplayShowTitleEnabled(false);
-        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-
-        mNavigationItemList = new ArrayList<SpinnerNavigationItem>();
-
-        mNavigationItemList.add(new SpinnerNavigationItem(getResources().getString(R.string.navigation_item_title_list),
-                R.drawable.ic_action_view_as_list, mATMListFragment));
-        mNavigationItemList.add(new SpinnerNavigationItem(getResources().getString(R.string.navigation_item_title_map),
-                R.drawable.ic_action_map, mMapFragment));
-
-        mNavigationAdapter = new NavigationAdapter(getApplicationContext(), mNavigationItemList);
-
-        mActionBar.setListNavigationCallbacks(mNavigationAdapter, this);
-        mActionBar.setDisplayHomeAsUpEnabled(true);
-        mActionBar.setHomeButtonEnabled(true);
     }
 
     private void handleIntent(Intent intent) {
@@ -168,14 +133,6 @@ public class MainActivity extends Activity implements ActionBar.OnNavigationList
 
     public LocationTracker getLocationTracker() {
         return mLocationTracker;
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
-        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, mNavigationAdapter.getItemsFragment(itemPosition));
-        fragmentTransaction.commit();
-        return true;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
